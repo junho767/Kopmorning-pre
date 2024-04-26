589e61dab3b0d0f5e1413e45be3461b15f87dc9d
leejunho <junho767@naver.com>
2024-04-26 19:56:31 +0900

    로그인 뒤엎기 전 마지막 저장

--- src/main/java/me/junholee/springbootdeveloper/SpringBootDeveloperApplication.java
+++ src/main/java/me/junholee/springbootdeveloper/SpringBootDeveloperApplication.java
@@ -10,11 +10,16 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @EnableJpaAuditing //created_at, update_at 자동 업데이트
 @SpringBootApplication
 @RequiredArgsConstructor
 public class SpringBootDeveloperApplication {
+    @Bean
+    public BCryptPasswordEncoder encoder(){
+        return new BCryptPasswordEncoder();
+    }
     public static void main(String[] args){
         SpringApplication.run(SpringBootDeveloperApplication.class, args);
     }
